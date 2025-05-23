6394a7fe1c1a38bb0749c87d3af8903362751df3
Jose <jose.zuniga1145@gmail.com>
2025-05-23 00:11:16 -0600

    fix: set Node.js version to 20 for Cloudflare Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   images: {
     domains: ['img.youtube.com'],
-  },
-  webpack: (config, { isServer }) => {
-    config.cache = false;
-    return config;
   },
 };
 
